e2e955e3d4989f555a56985ca3d3e9590da1d043
Your Name <you@example.com>
2019-02-01 15:51:27 +0000

    HomePage beginning

---
+++ public/build/js/homePage.js
@@ -0,0 +1,22 @@
+function adpaterALaTailleDeLaFenetre(){
+    var largeur = document.documentElement.clientWidth;
+    var hauteur = document.documentElement.clientHeight-document.getElementById('headerSize').clientHeight;
+    var centre = ((document.documentElement.clientHeight-document.getElementById('headerSize').clientHeight)/2).toString()+"px";
+    const source = document.getElementById('first-view-js');
+    source.style.height = hauteur+'px';
+    source.style.width = largeur+'px';
+    source.style.backgroundColor= 'purple';
+    source.style.textAlign='center';
+    source.style.paddingTop= centre;
+}
+
+function addEvent(element, type, listener){
+    if(element.addEventListener){
+        element.addEventListener(type, listener, false);
+    }
+}
+
+
+addEvent(window, "load", adpaterALaTailleDeLaFenetre);
+
+addEvent(window, "resize", adpaterALaTailleDeLaFenetre);
