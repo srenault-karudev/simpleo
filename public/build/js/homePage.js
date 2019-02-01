d2894b1a042ef33e8be1a9eb06e8fddec92c37dd
Your Name <you@example.com>
2019-02-01 17:29:01 +0000

    continue

--- public/build/js/homePage.js
+++ public/build/js/homePage.js
@@ -1,7 +1,7 @@
 function adpaterALaTailleDeLaFenetre(){
     var largeur = document.documentElement.clientWidth;
     var hauteur = document.documentElement.clientHeight-document.getElementById('headerSize').clientHeight;
-    var centre = ((document.documentElement.clientHeight-document.getElementById('headerSize').clientHeight)/2).toString()+"px";
+    var centre = (((document.documentElement.clientHeight-document.getElementById('headerSize').clientHeight)/2)-150).toString()+"px";
     const source = document.getElementById('first-view-js');
     source.style.height = hauteur+'px';
     source.style.width = largeur+'px';
