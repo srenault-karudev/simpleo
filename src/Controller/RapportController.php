b77db3310b2bd470f58e2a3abf207154076bbfbd
paraiso <adrien.paraiso@etu.u-pec.fr>
2019-02-08 15:06:28 +0000

    Header + Responsive + Dashboard + Onglets/liens fonctionnels

---
+++ src/Controller/RapportController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven
+ * Date: 2019-01-17
+ * Time: 11:55
+ */
+
+namespace App\Controller;
+
+
+use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class RapportController extends Controller
+{
+
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/Rapport", name="rapport")
+     */
+
+    public function index()
+    {
+
+        return $this->render('rapport.html.twig');
+
+    }
+}
---
+++ src/Controller/StockageController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven
+ * Date: 2019-01-17
+ * Time: 11:55
+ */
+
+namespace App\Controller;
+
+
+use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class StockageController extends Controller
+{
+
+    private $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @Route("/Stockage", name="stockage")
+     */
+
+    public function index()
+    {
+
+        return $this->render('stockage.html.twig');
+
+    }
+}
