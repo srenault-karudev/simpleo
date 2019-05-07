945f56a4ef00b9ab07fec2f9ab0bfad3fdfbb97b
Adrien PARAISO <adrien.paraiso@etu.u-pec.fr>
2019-05-07 07:17:14 +0000

    page d'accueil devis

---
+++ src/Repository/DevisRepository.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrienparaiso
+ * Date: 06/05/2019
+ * Time: 17:20
+ */
+
+namespace App\Repository;
+
+use App\Entity\User;
+
+class DevisRepository
+{
+    public function getDevis(User $user)
+    {
+    }
+}
